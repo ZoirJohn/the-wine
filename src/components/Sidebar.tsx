86f1e2a4dddc76cbfe62761275404f436451e436
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-04-01 13:30:06 +0500

    filter has been finished

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -15,17 +15,19 @@ const Sidebar = () => {
                                                 <path d='M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z' />
                                         </svg>
                                 </button>
-                                {isOpenFilter && (
-                                        <div className='bg-brownie z-10 w-full overflow-scroll max-md:h-screen max-md:fixed max-md:top-0 max-md:left-1/2 max-md:-translate-x-1/2 max-md:p-4 max-md:pt-8 '>
-                                                <span
-                                                        onClick={() => setIsOpenFilter(false)}
-                                                        className='relative max-md:before:absolute max-md:before:w-4 max-md:before:h-[1px] max-md:before:bg-white max-md:before:rotate-45 max-md:after:absolute max-md:after:w-4 max-md:after:h-[1px] max-md:after:bg-white max-md:after:-rotate-45 max-md:before:-top-4 max-md:after:-top-4'
-                                                ></span>
-                                                <Dropdown open={true} filterArray={['Белое', 'Красное', 'Розовое', 'Прочее']} name='Цвет' />
-                                                <Dropdown open={true} filterArray={['Брют', 'Десертное', 'Крепленное', 'Нон-Дозаж', 'полусладкое', 'полусухое', 'сухое']} name='Сладость' />
-                                                <Dropdown open={true} filterArray={'input'} name='Цена' />
-                                        </div>
-                                )}
+                                <div
+                                        className={`${
+                                                isOpenFilter ? 'block' : 'hidden'
+                                        } bg-brownie z-10 w-full overflow-auto max-md:h-screen max-md:fixed max-md:top-0 max-md:left-1/2 max-md:-translate-x-1/2 max-md:p-4 max-md:pt-8`}
+                                >
+                                        <span
+                                                onClick={() => setIsOpenFilter(false)}
+                                                className='relative max-md:before:absolute max-md:before:w-4 max-md:before:h-[1px] max-md:before:bg-white max-md:before:rotate-45 max-md:after:absolute max-md:after:w-4 max-md:after:h-[1px] max-md:after:bg-white max-md:after:-rotate-45 max-md:before:-top-4 max-md:after:-top-4'
+                                        ></span>
+                                        <Dropdown open={true} filterArray={['Белое', 'Красное', 'Розовое', 'Прочее']} name='Цвет' />
+                                        <Dropdown open={true} filterArray={['Брют', 'Десертное', 'Крепленное', 'Нон-Дозаж', 'полусладкое', 'полусухое', 'сухое']} name='Сладость' />
+                                        <Dropdown open={true} filterArray={'input'} name='Цена' />
+                                </div>
                         </aside>
                 </>
         )
